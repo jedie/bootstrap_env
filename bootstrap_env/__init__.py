ec4839dc85383f745329c11d71b15f2073f22f8f
JensDiemer <git@jensdiemer.de>
2015-06-05 21:59:12 +0200

    +__version__="0.5.0"

--- bootstrap_env/__init__.py
+++ bootstrap_env/__init__.py
@@ -14,7 +14,7 @@ from __future__ import print_function, absolute_import
 import warnings
 
 
-__version__="0.4.6"
+__version__="0.5.0"
 
 
 # Old API e.g.: bootstrap_env.create_bootstrap.generate_bootstrap()
