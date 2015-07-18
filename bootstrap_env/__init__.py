e62c47c878aa2123d87f3fa62475b58bb44070ba
JensDiemer <git@jensdiemer.de>
2015-07-18 20:53:53 +0200

    Update to pip 7.1.0

--- bootstrap_env/__init__.py
+++ bootstrap_env/__init__.py
@@ -14,7 +14,7 @@ from __future__ import print_function, absolute_import
 import warnings
 
 
-__version__="0.5.0"
+__version__="0.5.1"
 
 
 # Old API e.g.: bootstrap_env.create_bootstrap.generate_bootstrap()
