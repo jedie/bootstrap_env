8ecfa3c8919d71ba08840ac846e182478bdd20eb
JensDiemer <git@jensdiemer.de>
2015-08-26 10:31:52 +0200

    work for Py2.4 and Py2.7 + more info on missing file

--- bootstrap_env/boot.cmd
+++ bootstrap_env/boot.cmd
@@ -4,14 +4,19 @@ title "%~0"
 
 set python=C:\Python34\python.exe
 if NOT exist %python% (
-    echo ERROR: '%python%' doesn't exists?!?
-    pause
-    exit 1
+    set python=C:\Python27\python.exe
+    if NOT exist %python% (
+        echo ERROR: can't find 'python.exe' !
+        echo Please edit this file: "%~dp0"
+        pause
+        exit 1
+    )
 )
 
 set boot_script=boot.py
 if NOT exist %boot_script% (
     echo ERROR: '%boot_script%' doesn't exists?!?
+    echo Please edit this file: "%~dp0"
     pause
     exit 1
 )
@@ -20,6 +25,7 @@ set destination=%APPDATA%\bootstrap_env
 mkdir %destination%
 if NOT exist %destination% (
     echo ERROR: '%destination%' doesn't exists?!?
+    echo Please edit this file: "%~dp0"
     pause
     exit 1
 )
