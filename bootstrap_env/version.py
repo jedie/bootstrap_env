9447595dcc698603ccd41579eb9bb2ba96f659a1
JensDiemer <git@jensdiemer.de>
2019-01-08 09:35:05 +0100

    release as v1.0.1

--- bootstrap_env/version.py
+++ bootstrap_env/version.py
@@ -4,11 +4,11 @@
     bootstrap-env
     ~~~~~~~~~~~~~
 
-    :copyleft: 2018 by bootstrap-env team, see AUTHORS for more details.
     :created: 2018 by JensDiemer.de
     :license: GNU GPL v3 or above, see LICENSE for more details.
+    :copyleft: 2018-2019 by bootstrap-env team, see AUTHORS for more details.
 """
 
 # https://pypi.org/project/bootstrap_env/#history
 # https://github.com/jedie/bootstrap_env#history
-__version__ = "1.0.0rc18"
+__version__ = "1.0.1"
